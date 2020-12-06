34262f59b88fd725a35a365b67312fd22a8a1bc3
Devora Nagel <devoranagel@gmail.com>
2020-12-06 13:40:46 -0600

    added SettingsActivity

--- app/src/main/java/com/example/thirteenstones/activities/SplashActivity.java
+++ app/src/main/java/com/example/thirteenstones/activities/SplashActivity.java
@@ -2,13 +2,14 @@ package com.example.thirteenstones.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.preference.PreferenceManager;
 
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 
-import static androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY;
-import static androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM;
+import com.example.thirteenstones.R;
+import com.example.thirteenstones.lib.Utils;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -16,8 +17,8 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        AppCompatDelegate.setDefaultNightMode
-                (Build.VERSION.SDK_INT < 28 ? MODE_NIGHT_AUTO_BATTERY : MODE_NIGHT_FOLLOW_SYSTEM);
+        PreferenceManager.setDefaultValues(this, R.xml.root_preferences, false);
+        Utils.setNightModeOnOffFromPreferenceValue(getApplicationContext(), getString(R.string.night_mode_key));
 
         startActivity (new Intent(getApplicationContext (), MainActivity.class));
         finish ();
